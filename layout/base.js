46736871d4939ee32ac60ca70bb229409fd62cd0
KENZ <KENZ.gelsoft@gmail.com>
2026-06-07 10:27:21 +0900

    テーブルや引用を見やすいよう適当にスタイル

--- layout/base.js
+++ layout/base.js
@@ -14,8 +14,37 @@ export default layout(
         <title>${page.title}</title>
         <style type="text/css">
           img {
-            max-width: 100vw;
+            max-width: 100%;
             max-height: 66vh;
+          }
+          pre:has(code) {
+            background-color: black;
+            color: white;
+            padding: 0.75em;
+          }
+
+          table {
+            border-collapse: collapse;
+            width: 100%;
+            margin: 1.5rem 0;
+            font-size: 0.9rem;
+          }
+          th {
+            background-color: lightgray;
+            text-align: left;
+          }
+          th,
+          td {
+            padding: 0.75rem;
+            border: 1px solid black;
+          }
+
+          blockquote {
+            margin: 1.5rem 0;
+            padding-left: 1rem;
+            border-left: 4px solid lightgray;
+            color: gray;
+            font-style: italic;
           }
         </style>
       </head>
