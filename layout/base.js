a558dfde5311f54c6444439b9fba031518a3f7c0
KENZ <KENZ.gelsoft@gmail.com>
2026-06-08 00:14:39 +0900

    viewportを設定

--- layout/base.js
+++ layout/base.js
@@ -11,6 +11,7 @@ export default layout(
     <html lang="ja">
       <head>
         <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>${page.title}</title>
         <style type="text/css">
           img {
