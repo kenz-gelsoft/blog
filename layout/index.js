458cb241004fc7bff5348748bbe33fdc9a1f435d
KENZ <KENZ.gelsoft@gmail.com>
2026-06-08 01:04:09 +0900

    記事一覧をソート・絞り込み

--- layout/index.js
+++ layout/index.js
@@ -6,7 +6,10 @@ export default layout(
     layout: "base", // 最終的に最外殻の base layout に包む
   },
   (page) => {
-    const posts = page.content; // ここに配列が入っている
+    const formatter = new Intl.DateTimeFormat("ja-JP");
+    const posts = page.content
+      .sort((a, b) => b.date - a.date)
+      .filter((p) => p.published !== false); // ここに配列が入っている
     return html`
       <section class="archive">
         <h1>${page.title}</h1>
@@ -15,7 +18,7 @@ export default layout(
           ${posts.map(
             (post) => html`
               <li>
-                <time>${post.date}</time>
+                <time>${formatter.format(post.date)}</time>
                 <a href="/${post.slug || ""}">${post.title}</a>
                 <p>${post.description || ""}</p>
               </li>
