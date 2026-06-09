f9e044ee546cc0e796e558c86ba8acb05c28aed4
KENZ <KENZ.gelsoft@gmail.com>
2026-06-09 10:46:30 +0900

    ルーティングロジックを Router クラスに切り出し

--- js/preview.js
+++ js/preview.js
@@ -6,32 +6,57 @@ import { allPathsAndPosts, resolveChain } from "./engine.js";
 
 globalThis.BASE = "";
 
-const readFile = async (path) => (await fetch(`/${path}`)).text();
-
 async function renderPage() {
   // window.location.pathname から現在のパスを取得 (例: "/posts/tech/rust" -> "posts/tech/rust")
   // 先頭と末尾のロケールやスラッシュを掃除
   const pathName = window.location.pathname.replace(/^\/|\/$|index.html$/g, "");
   const currentPath = pathName === "" ? "index" : pathName;
 
-  const page = {
-    ...config,
-    base: globalThis.BASE,
-  };
+  const router = new Router({
+    readFile: async (path) => (await fetch(`/${path}`)).text(),
+    render: (result) => render(result, document.body),
+  });
+  await router.renderPath(currentPath);
+}
 
-  if (currentPath !== "index") {
-    const postLayout = await mdPost(`${currentPath}.md`, readFile);
-    const finalHtml = await resolveChain(postLayout(page));
-    render(finalHtml, document.body);
-  } else {
-    // 一覧ページを表示するときだけ、全ファイルの「Frontmatter（メタデータ）だけ」を非同期で回収する
-    const { allPosts } = await allPathsAndPosts("paths.txt", readFile);
-    const site = Object.assign(Object.create(page), {
-      pages: allPosts,
-    });
+class Router {
+  _readFile = null;
+  _render = null;
 
-    const finalHtml = await resolveChain(index(site));
-    render(finalHtml, document.body);
+  constructor({ readFile, render }) {
+    this._readFile = readFile;
+    this._render = render;
+  }
+
+  // キャッシュ
+  _allPosts = null;
+  async allPosts() {
+    if (this._allPosts == null) {
+      const { allPosts } = await allPathsAndPosts("paths.txt", this._readFile);
+      this._allPosts = allPosts;
+    }
+    return this._allPosts;
+  }
+
+  async renderPath(path, renderInternal) {
+    const page = {
+      ...config,
+      base: globalThis.BASE,
+    };
+
+    if (path !== "index") {
+      const postLayout = await mdPost(`${path}.md`, this._readFile);
+      const finalHtml = await resolveChain(postLayout(page));
+      this._render(finalHtml);
+    } else {
+      const site = Object.assign(Object.create(page), {
+        // 一覧ページを表示するときだけ、全ファイルの「Frontmatter（メタデータ）だけ」を非同期で回収する
+        pages: await this.allPosts(),
+      });
+
+      const finalHtml = await resolveChain(index(site));
+      this._render(finalHtml);
+    }
   }
 }
 
